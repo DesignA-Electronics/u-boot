0ff2de6881f8995fc60c7fd8502f89e22760bf36
Andre Renaud <andre@ignavus.net>
2024-07-29 12:39:12 +1200

    KSZ9896: Fix up single-led mode and MMD access
    LED behaviour now matches rev2 Marvell behaviour

--- board/designa/snappermx6/ksz9896.c
+++ board/designa/snappermx6/ksz9896.c
@@ -7,42 +7,38 @@
 #include <linux/errno.h>
 #include <i2c.h>
 
- #define KSZ9896_MMDACR                                         0x0D
- #define KSZ9896_MMDAADR                                        0x0E
+// MMD access
+#define KSZ9896_PHY_MMD_SETUP 0x11a
+#define KSZ9896_PHY_MMD_DATA 0x11c
 
-  //PHY MMD Setup register
- #define KSZ9896_MMDACR_FUNC                                    0xC000
- #define KSZ9896_MMDACR_FUNC_ADDR                               0x0000
- #define KSZ9896_MMDACR_FUNC_DATA_NO_POST_INC                   0x4000
- #define KSZ9896_MMDACR_FUNC_DATA_POST_INC_RW                   0x8000
- #define KSZ9896_MMDACR_FUNC_DATA_POST_INC_W                    0xC000
- #define KSZ9896_MMDACR_DEVAD                                   0x001F
+//PHY MMD Setup register
+#define KSZ9896_MMDACR_FUNC                                    0xC000
+#define KSZ9896_MMDACR_FUNC_ADDR                               0x0000
+#define KSZ9896_MMDACR_FUNC_DATA_NO_POST_INC                   0x4000
+#define KSZ9896_MMDACR_FUNC_DATA_POST_INC_RW                   0x8000
+#define KSZ9896_MMDACR_FUNC_DATA_POST_INC_W                    0xC000
+#define KSZ9896_MMDACR_DEVAD                                   0x001F
 
 // PHY status register
 #define KSZ9896_PHY_BASIC_STATUS								0x0102
 #define KSZ9896_PHY_LINK_PARTNER_STATUS							0x010A
 #define KSZ9896_PHY_1000BT_STATUS								0x0114
 
- //MMD LED Mode register
- #define KSZ9896_MMD_LED_MODE_LED_MODE                          0x0010
- #define KSZ9896_MMD_LED_MODE_LED_MODE_TRI_COLOR_DUAL           0x0000
- #define KSZ9896_MMD_LED_MODE_LED_MODE_SINGLE                   0x0010
- #define KSZ9896_MMD_LED_MODE_RESERVED                          0x000F
- #define KSZ9896_MMD_LED_MODE_RESERVED_DEFAULT                  0x0001
+//MMD LED Mode register
+#define KSZ9896_MMD_LED_MODE_LED_MODE                          0x0010
+#define KSZ9896_MMD_LED_MODE_LED_MODE_TRI_COLOR_DUAL           0x0000
+#define KSZ9896_MMD_LED_MODE_LED_MODE_SINGLE                   0x0010
+#define KSZ9896_MMD_LED_MODE_RESERVED                          0x000F
+#define KSZ9896_MMD_LED_MODE_RESERVED_DEFAULT                  0x0001
   
- //MMD EEE Advertisement register
- #define KSZ9896_MMD_EEE_ADV_1000BT_EEE_EN                      0x0004
- #define KSZ9896_MMD_EEE_ADV_100BT_EEE_EN                       0x0002
-
-
- //KSZ9896 MMD registers
- #define KSZ9896_MMD_LED_MODE                                   0x02, 0x00
- #define KSZ9896_MMD_EEE_ADV                                    0x07, 0x3C
+//KSZ9896 MMD registers
+#define KSZ9896_MMD_LED_MODE                                   0x02, 0x00
+#define KSZ9896_MMD_EEE_ADV                                    0x07, 0x3C
 
 static int ksz9896_write16(struct udevice *dev, uint8_t port, uint16_t reg, uint16_t val)
 {
 	uint8_t data[2] = {val >> 8, val};
-	if (dm_i2c_write(dev, port << 12 | reg, data, 2)) {
+	if (dm_i2c_write(dev, (port << 12) | reg, data, 2)) {
 		printf("cannot write 0x%x=0x%04x\n", reg, val);
 		return -1;
 	}
@@ -52,16 +48,26 @@ static int ksz9896_write16(struct udevice *dev, uint8_t port, uint16_t reg, uint
 static int ksz9896_read16(struct udevice *dev, uint8_t port, uint16_t reg)
 {
 	uint16_t data;
-	if (dm_i2c_read(dev, port << 12 | reg, (uint8_t *)&data, 2)) {
+	if (dm_i2c_read(dev, (port << 12) | reg, (uint8_t *)&data, 2)) {
 		return -1;
 	}
 	return __be16_to_cpu(data);
 }
 
+static int ksz9896_write32(struct udevice *dev, uint8_t port, uint16_t reg, uint32_t val)
+{
+	uint8_t data[4] = {val >> 24, val >> 16, val >> 8, val};
+	if (dm_i2c_write(dev, (port << 12) | reg, data, sizeof(data))) {
+		printf("cannot write 0x%x=0x%04x\n", reg, val);
+		return -1;
+	}
+	return 0;
+}
+
 static uint32_t ksz9896_read32(struct udevice *dev, uint8_t port, uint16_t reg)
 {
 	uint32_t val;
-	if (dm_i2c_read(dev, port << 12 | reg, (uint8_t *)&val, sizeof(val))) {
+	if (dm_i2c_read(dev, (port << 12) | reg, (uint8_t *)&val, sizeof(val))) {
 		return -1;
 	}
     return __be32_to_cpu(val);
@@ -70,34 +76,51 @@ static uint32_t ksz9896_read32(struct udevice *dev, uint8_t port, uint16_t reg)
 static int ksz9896_write_mmd(struct udevice *dev, uint8_t port, uint8_t dev_addr, uint16_t reg_addr, uint16_t data)
 {
     //Select register operation
-    ksz9896_write16(dev, port, KSZ9896_MMDACR,
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_SETUP,
        KSZ9896_MMDACR_FUNC_ADDR | (dev_addr & KSZ9896_MMDACR_DEVAD));
-  
+
     //Write MMD register address
-    ksz9896_write16(dev, port, KSZ9896_MMDAADR, reg_addr);
-  
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_DATA, reg_addr);
+
     //Select data operation
-    ksz9896_write16(dev, port, KSZ9896_MMDACR,
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_SETUP,
        KSZ9896_MMDACR_FUNC_DATA_NO_POST_INC | (dev_addr & KSZ9896_MMDACR_DEVAD));
-  
+
     //Write the content of the MMD register
-    ksz9896_write16(dev, port, KSZ9896_MMDAADR, data);
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_DATA, data);
 
 	return 0;
+}
+
+static int ksz9896_read_mmd(struct udevice *dev, uint8_t port, uint8_t dev_addr, uint16_t reg_addr)
+{
+    //Select register operation
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_SETUP,
+       KSZ9896_MMDACR_FUNC_ADDR | (dev_addr & KSZ9896_MMDACR_DEVAD));
+
+    //Write MMD register address
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_DATA, reg_addr);
+
+    //Select data operation
+    ksz9896_write16(dev, port, KSZ9896_PHY_MMD_SETUP,
+       KSZ9896_MMDACR_FUNC_DATA_NO_POST_INC | (dev_addr & KSZ9896_MMDACR_DEVAD));
+
+    // Read the content of the MMD register
+    return ksz9896_read16(dev, port, KSZ9896_PHY_MMD_DATA);
 }
 
 int ksz9896_init(void)
 {
 	struct udevice *dev;
 	uint32_t id = 0;
-    uint8_t xmii_ctrl_1 = 0x18;
+	uint8_t xmii_ctrl_1 = 0x18;
 
 	if (i2c_get_chip_for_busnum(0, 0x5f, 2, &dev) < 0)
 		return -1;
 
 	id = ksz9896_read32(dev, 0, 0);
 
-    if (id != 0x00989600) {
+	if (id != 0x00989600) {
 		printf("KSZ9698: invalid ID: %08x\n", id);
 		return 1;
 	}
@@ -108,41 +131,45 @@ int ksz9896_init(void)
 	// Apply errata as per.
 	// See 'KSZ9896C Silicon Errata and Data Sheet Clarification'
 	// https://ww1.microchip.com/downloads/en/DeviceDoc/80000757C.pdf
-	for(int port = 1; port <= 5; port++) {
-       //Improve PHY receive performance (silicon errata workaround 1)
-       ksz9896_write_mmd(dev, port, 0x01, 0x6F, 0xDD0B);
-       ksz9896_write_mmd(dev, port, 0x01, 0x8F, 0x6032);
-       ksz9896_write_mmd(dev, port, 0x01, 0x9D, 0x248C);
-       ksz9896_write_mmd(dev, port, 0x01, 0x75, 0x0060);
-       ksz9896_write_mmd(dev, port, 0x01, 0xD3, 0x7777);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x06, 0x3008);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x08, 0x2001);
-  
-       //Improve transmit waveform amplitude (silicon errata workaround 2)
-       ksz9896_write_mmd(dev, port, 0x1C, 0x04, 0x00D0);
-  
-       //EEE must be manually disabled (silicon errata workaround 3)
-       ksz9896_write_mmd(dev, port, KSZ9896_MMD_EEE_ADV, 0);
-  
-       //Adjust power supply settings (silicon errata workaround 6)
-       ksz9896_write_mmd(dev, port, 0x1C, 0x13, 0x6EFF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x14, 0xE6FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x15, 0x6EFF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x16, 0xE6FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x17, 0x00FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x18, 0x43FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x19, 0xC3FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x1A, 0x6FFF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x1B, 0x07FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x1C, 0x0FFF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x1D, 0xE7FF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x1E, 0xEFFF);
-       ksz9896_write_mmd(dev, port, 0x1C, 0x20, 0xEEEE);
-  
-       //Select tri-color dual-LED mode (silicon errata workaround 14)
-       ksz9896_write_mmd(dev, port, KSZ9896_MMD_LED_MODE,
-          KSZ9896_MMD_LED_MODE_LED_MODE_TRI_COLOR_DUAL |
-          KSZ9896_MMD_LED_MODE_RESERVED_DEFAULT);
+	for (int port = 1; port <= 5; port++) {
+		// Improve PHY receive performance (silicon errata workaround 1)
+		ksz9896_write_mmd(dev, port, 0x01, 0x6F, 0xDD0B);
+		ksz9896_write_mmd(dev, port, 0x01, 0x8F, 0x6032);
+		ksz9896_write_mmd(dev, port, 0x01, 0x9D, 0x248C);
+		ksz9896_write_mmd(dev, port, 0x01, 0x75, 0x0060);
+		ksz9896_write_mmd(dev, port, 0x01, 0xD3, 0x7777);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x06, 0x3008);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x08, 0x2001);
+
+		// Improve transmit waveform amplitude (silicon errata workaround 2)
+		ksz9896_write_mmd(dev, port, 0x1C, 0x04, 0x00D0);
+
+		// EEE must be manually disabled (silicon errata workaround 3)
+		ksz9896_write_mmd(dev, port, KSZ9896_MMD_EEE_ADV, 0);
+
+		// Adjust power supply settings (silicon errata workaround 6)
+		ksz9896_write_mmd(dev, port, 0x1C, 0x13, 0x6EFF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x14, 0xE6FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x15, 0x6EFF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x16, 0xE6FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x17, 0x00FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x18, 0x43FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x19, 0xC3FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x1A, 0x6FFF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x1B, 0x07FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x1C, 0x0FFF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x1D, 0xE7FF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x1E, 0xEFFF);
+		ksz9896_write_mmd(dev, port, 0x1C, 0x20, 0xEEEE);
+
+
+		// Select single-color mode (silicon errata workaround 14)
+		// https://microchipsupport.force.com/s/article/Single-LED-mode-in-the-KSZ9897-and-KSZ9893-Ethernet-switch-families
+		// https://ww1.microchip.com/downloads/aemDocuments/documents/UNG/ProductDocuments/Errata/KSZ9896C-Errata-DS80000757.pdf
+		ksz9896_write_mmd(dev, port, KSZ9896_MMD_LED_MODE, KSZ9896_MMD_LED_MODE_LED_MODE_SINGLE | KSZ9896_MMD_LED_MODE_RESERVED_DEFAULT);
+
+		// This write must be 32-bit because of a separate errata issue
+		ksz9896_write32(dev, port, 0x13C, 0xfa000300);
 	}
 
 	return 0;
@@ -151,7 +178,7 @@ int ksz9896_init(void)
 static uint32_t ksz9896_read_mib(struct udevice *dev, uint8_t port, uint8_t mib_index)
 {
 	uint8_t ctrl[4] = {0x2, mib_index, 0, 0};
-	if (dm_i2c_write(dev, 0x500 | port << 12, ctrl, 4)) {
+	if (dm_i2c_write(dev, 0x500 | (port << 12), ctrl, 4)) {
 		printf("cannot write %d 0x%x\n", port, mib_index);
 		return -1;
 	}
